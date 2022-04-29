3d8a81fcf32d8f30d1f807be77053d8ba5fc4482
Catcheryp <2281718195@qq.com>
2022-04-29 23:36:07 +0800

    fastjson-demo01测试

--- FastJson-Labs/fastjson-demo01/src/main/java/Demo.java
+++ FastJson-Labs/fastjson-demo01/src/main/java/Demo.java
@@ -1,12 +1,24 @@
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.Feature;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 
 public class Demo {
     public static void main(String[] args) {
-        Student student = new Student();
-        student.setAge(5);
-        student.setName("Tom");
-        student.setTelephone("123456");
-        String jsonString = JSON.toJSONString(student);
-        System.out.println(jsonString);
+        // Student student = new Student();
+        // student.setAge(5);
+        // student.setName("Tom");
+        // student.setTelephone("123456");
+        // // 序列化
+        // // String jsonString = JSON.toJSONString(student);
+        // // String jsonString = JSON.toJSONString(student, SerializerFeature.WriteClassName);
+        // // System.out.println(jsonString);
+
+        // 反序列化
+        String jsonString = "{\"@type\":\"Student\",\"age\":5,\"name\":\"Tom\",\"telephone\":\"123456\", \"properties\":{}}";
+        // Student obj = JSON.parseObject(jsonString, Student.class, Feature.SupportNonPublicField);
+        // Student obj = JSON.parseObject(jsonString, Student.class);
+        Object obj = JSON.parseObject(jsonString, Object.class);
+        System.out.println(obj);
+        System.out.println(obj.getClass());
     }
 }
--- FastJson-Labs/fastjson-demo01/src/main/java/Student.java
+++ FastJson-Labs/fastjson-demo01/src/main/java/Student.java
@@ -1,34 +1,58 @@
+import java.util.Properties;
+
 public class Student {
     private String name;
     private int age;
     private String telephone;
+    private Properties properties;
+
+    public Student() {
+        System.out.println("调用无参构造函数");
+    }
 
     public String getName() {
+        System.out.println("调用getName");
         return name;
     }
 
     public void setName(String name) {
+        System.out.println("调用setName");
         this.name = name;
     }
 
     public int getAge() {
+        System.out.println("调用setName");
         return age;
     }
 
     public void setAge(int age) {
+        System.out.println("调用setAge");
         this.age = age;
     }
 
     public String getTelephone() {
+        System.out.println("调用getTelephone");
         return telephone;
     }
 
     public void setTelephone(String telephone) {
+        System.out.println("调用setTelephone");
         this.telephone = telephone;
     }
 
+    public Properties getProperties() {
+        System.out.println("调用getProperties");
+        return properties;
+    }
+
+    // public void setProperties(Properties properties) {
+    //     System.out.println("调用setProperties");
+    //     this.properties = properties;
+    // }
+
     @Override
     public String toString() {
+        System.out.println("调用toString");
         return "Student{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
